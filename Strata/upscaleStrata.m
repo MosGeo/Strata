2225e0048e06d05fca795d17122487930d17a3e3
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2018-11-19 17:19:48 -0800

    Finished upscaling function (Mode)

--- Strata/upscaleStrata.m
+++ Strata/upscaleStrata.m
@@ -1,15 +1,29 @@
-function strata = upscaleStrata(strata, smoothingInterval)
+function strata = upscaleStrata(strata, smoothingInterval, type)
+%% UPSCALESTRATA  Upscale classificaiton
+%
+% strata:           Strataigraphic table (includes lithology, thickness)
+%
+% Mustafa Al Ibrahim @ 2018
+% Mustafa.Geoscientist@outlook.com
 
-    classification = strata.lithology;
-    padSize = (smoothingInterval - 1) / 2;   
-    
+%% Preprocessing
 
-    
-    B = padarray(classification,padSize,'replicate');
-    for j = 1+padSize:padSize+numel(classification)
-        classification(j-padSize) = mode(B(j-padSize: j+padSize));
-    end
-        
-    strata.lithology = classification;
+% Defaults
+if ~exist('smoothingInterval', 'var'); smoothingInterval = 1; end
+
+% Assertions
+assert(exist('strata', 'var')==true, 'strata must be provided');
+
+%% Main
+
+classification = strata.lithology;
+padSize = (smoothingInterval - 1) / 2;   
+
+B = padarray(classification,padSize,'replicate');
+for j = 1+padSize:padSize+numel(classification)
+    classification(j-padSize) = mode(B(j-padSize: j+padSize));
+end
+
+strata.lithology = classification;
 
 end
