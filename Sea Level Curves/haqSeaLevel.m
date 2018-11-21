970d94125ac71a4893e37e8c31f401de92315706
Mustafa Al Ibrahim <MosGeo@users.noreply.github.com>
2018-11-21 10:42:08 -0800

    sea level function

--- Sea Level Curves/haqSeaLevel.m
+++ Sea Level Curves/haqSeaLevel.m
@@ -1,5 +1,48 @@
-function [seaLevelAge, seaLevelHeight] =  haqSeaLevel(minAge, maxAge, dt)
+function [age, height] =  haqSeaLevel(minAge, maxAge, dt, isPlot)
+
+%% Preprocessing
+
+% Defaults
+if ~exist('minAge', 'var'); minAge = -inf; end
+if ~exist('maxAge', 'var'); maxAge = inf; end
+if ~exist('maxAge', 'var'); dt = nan; end
+if ~exist('isPlot', 'var'); isPlot = false; end
 
 
+%% Main
+
+% Data
+fileNames = {'Haq87_Longterm.dat', 'Haq87_Longterm_Normalized.dat', ...
+    'Haq87_HighResolution.dat', 'Haq87_HighResolution_Filtered.dat'};
+
+i = 2;
+% Import data
+rawData = importdata(fileNames{i});
+age = rawData(:,1);
+height = rawData(:,2);
+
+% Filter data
+indexToKeep = age>=minAge & age<=maxAge;
+age = age(indexToKeep);
+height = height(indexToKeep);
+
+% Resample if needed
+if exist('dt', 'var') && ~isnan(dt)
+    ageResampled = min(age):dt:max(age);
+    heightResampled = interp1(age,height, ageResampled);
+    age = ageResampled;
+    height = heightResampled;
+end
+
+
+% Plotting
+if isPlot
+    figure('Color', 'White')
+    plot(height, age, 'LineWidth', 2)
+    set(gca, 'yDir', 'reverse')
+    ylim([min(age), max(age)])
+    xlabel('Age')
+    ylabel('Height')
+end
 
 end
--- main.m
+++ main.m
@@ -82,7 +82,7 @@ seaLevelAge = 0:.01:maxAge;
 seaLevelHeight = sin(seaLevelAge/20) +  .25*sin(seaLevelAge/5);
 markovMatrices{1} = [.1 .4 .5; .1 .5 .4; 0 .3 .7];   % Shallow
 markovMatrices{2} = [.7 .2 .1; .4 .5 .1; .4 .4 .2];  % Deep
-depositionalRates = [1, 1, 1];
+depositionalRates = [1, 1, -1];
 
 % Simulate
 strata = simulateStrata(markovMatrices, age, seaLevelAge, seaLevelHeight, depositionalRates);
